43f80a035599fe7ffaa2c8adbdbba4fc01ee9953
Nihar Darji <54477606+nihardarji@users.noreply.github.com>
2021-07-02 01:53:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreditCardForm from "./components/CreditCardForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CreditCardForm />
+    </div>
+  );
+}
---
+++ src/components/CreditCardForm.js
@@ -0,0 +1,125 @@
+import React from "react";
+import useForm from "../useForm";
+import { Button, Form, Alert, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CreditCardForm.css";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+
+const CreditCardForm = () => {
+  const { handleChange, handleFocus, handleSubmit, values, errors } = useForm();
+
+  return (
+    <div>
+      <div className="container">
+        <div className="box justify-content-center align-items-center">
+          <div className="formDiv">
+            <div className="creditCard">
+              <Cards
+                cvc={values.cardSecurityCode}
+                expiry={values.cardExpiration}
+                focused={values.focus}
+                name={values.cardName}
+                number={values.cardNumber}
+              />
+            </div>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  onInput={(e) => {
+                    e.target.value = e.target.value.match("^[a-zA-Z-]*");
+                  }}
+                  id="cardName"
+                  data-testid="cardName"
+                  name="cardName"
+                  placeholder="Cardholder Name"
+                  value={values.cardName}
+                  onChange={handleChange}
+                  onFocus={handleFocus}
+                  isValid={errors.cname}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  type="number"
+                  onInput={(e) => {
+                    e.target.value = Math.max(0, parseInt(e.target.value))
+                      .toString()
+                      .slice(0, 16);
+                  }}
+                  id="cardNumber"
+                  data-testid="cardNumber"
+                  name="cardNumber"
+                  placeholder="Card Number"
+                  value={values.cardNumber}
+                  onChange={handleChange}
+                  onFocus={handleFocus}
+                  isValid={errors.cnumber}
+                />
+              </Form.Group>
+              <Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      id="cardExpiration"
+                      data-testid="cardExpiration"
+                      name="cardExpiration"
+                      placeholder="Expiration Date (MM/YY)"
+                      pattern="^(0[1-9]|1[0-2])\/?([0-9]{2})$"
+                      value={values.cardExpiration}
+                      onChange={handleChange}
+                      onFocus={handleFocus}
+                      isValid={errors.cexp}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Control
+                      type="number"
+                      id="cardSecurityCode"
+                      onInput={(e) => {
+                        e.target.value = Math.max(0, parseInt(e.target.value))
+                          .toString()
+                          .slice(0, 3);
+                      }}
+                      data-testid="cardSecurityCode"
+                      name="cardSecurityCode"
+                      placeholder="Security Code"
+                      value={values.cardSecurityCode}
+                      onChange={handleChange}
+                      onFocus={handleFocus}
+                      isValid={errors.ccvv}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Button
+                size={"block"}
+                data-testid="validateButton"
+                id="validateButton"
+                type="submit"
+              >
+                Validate
+              </Button>
+            </Form>
+          </div>
+          <Alert
+            id="alertMessage"
+            data-testid="alertMessage"
+            variant={errors.variant}
+            show={errors.show}
+          >
+            {errors.message}
+          </Alert>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreditCardForm;
